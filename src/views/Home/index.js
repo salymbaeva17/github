127a8eacb1bdeb96f2b6a4535f479c3cda4e35cc
Aitolkun Salymbaeva <salymbaeva17@gmail.com>
2021-09-25 17:11:47 +0600

    home

--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -27,8 +27,8 @@ const Home = () => {
     };
 
     TxtRotate.prototype.tick = function() {
-        var i = this.loopNum % this.toRotate.length;
-        var fullTxt = this.toRotate[i];
+        let i = this.loopNum % this.toRotate.length;
+        let fullTxt = this.toRotate[i];
 
         if (this.isDeleting) {
             this.txt = fullTxt.substring(0, this.txt.length - 1);
@@ -38,8 +38,8 @@ const Home = () => {
 
         this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
 
-        var that = this;
-        var delta = 300 - Math.random() * 100;
+        let that = this;
+        let delta = 300 - Math.random() * 100;
 
         if (this.isDeleting) { delta /= 2; }
 
@@ -58,15 +58,15 @@ const Home = () => {
     };
 
     window.onload = function() {
-        var elements = document.getElementsByClassName('txt-rotate');
-        for (var i=0; i<elements.length; i++) {
-            var toRotate = elements[i].getAttribute('data-rotate');
-            var period = elements[i].getAttribute('data-period');
+        let elements = document.getElementsByClassName('txt-rotate');
+        for (let i=0; i<elements.length; i++) {
+            let toRotate = elements[i].getAttribute('data-rotate');
+            let period = elements[i].getAttribute('data-period');
             if (toRotate) {
                 new TxtRotate(elements[i], JSON.parse(toRotate), period);
             }
         }
-        var css = document.createElement("style");
+        let css = document.createElement("style");
         css.type = "text/css";
         css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #666 }";
         document.body.appendChild(css);
