35281fc190549d0ce6c3512d72a3310d0e950f93
Aitolkun Salymbaeva <salymbaeva17@gmail.com>
2021-09-25 15:27:29 +0600

    readme

--- src/views/Readme/index.js
+++ src/views/Readme/index.js
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import Layout from "../../components/Layout";
 import {useParams} from "react-router-dom";
+import axios from "axios";
+import Markdown from "markdown-to-jsx";
 
 const Readme = () => {
-    const [username, project] = useParams()
+    const {username, project} = useParams()
+    const [readme, setReadme] = useState("")
+    useEffect(() => {
+        axios(`https://api.github.com/repos/${username}/${project}/readme`, {
+            headers: {Accept: "application/vnd.github.VERSION.raw"}
+        }).then(({data}) => setReadme(data))
+    }, [project, username])
 
     return (
-        <Layout>
-            {}
+        <Layout username={username}>
+            <Markdown className="readme" >{readme}</Markdown>
         </Layout>
     );
 };
