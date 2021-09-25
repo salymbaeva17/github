ed9e634ebf6b402321b8ca4c3a59a1e6261bdfc8
Aitolkun Salymbaeva <salymbaeva17@gmail.com>
2021-09-25 15:27:15 +0600

    home

--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -8,13 +8,20 @@ const Home = () => {
         setUsername(e.target.value)
     }
     const handlePress = (e) => {
-        if (e.key === "Enter" && username.trim()){
-            history.push(`/${username}`)
+        if (e.key === "Enter" && username.trim()) {
+           handleClick()
         }
     }
+    const handleClick = () =>{
+        username.trim() && history.push(`/${username}`)
+    }
     return (
-        <div>
-            <input type="text" onChange={handleChange} onKeyPress={handlePress}/>
+        <div className="homepage w-100 vh-100">
+            <div className="w-25 input-group mb-3">
+                <input type="text" onChange={handleChange} onKeyPress={handlePress} className="form-control"
+                       placeholder="Enter username" aria-describedby="button-addon2"/>
+                <button className="btn btn-success" onClick={handleClick} type="button" id="button-addon2">Search</button>
+            </div>
         </div>
     )
 }
