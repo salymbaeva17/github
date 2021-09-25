d620f3b9ea0aa35df34e5cccd8290366cc75d241
Aitolkun Salymbaeva <salymbaeva17@gmail.com>
2021-09-25 16:52:36 +0600

    finishing2

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -7,16 +7,16 @@ const Footer = () => {
                 <span>&copy;All rights reserved 2021</span>
                 <div className="social-media">
                     <span className="mx-3">
-                        <a className="sm-icon" href="https://github.com/salymbaeva17" target="_blank"><i className='bx bxl-github'/></a>
+                        <a className="sm-icon" rel="noreferrer" href="https://github.com/salymbaeva17" target="_blank"><i className='bx bxl-github'/></a>
                     </span>
                     <span className="mx-3">
-                        <a className="sm-icon" href="https://www.linkedin.com/in/aitolkun-salymbaeva" target="_blank"><i className='bx bxl-linkedin'/></a>
+                        <a className="sm-icon" rel="noreferrer" href="https://www.linkedin.com/in/aitolkun-salymbaeva" target="_blank"><i className='bx bxl-linkedin'/></a>
                     </span>
                     <span className="mx-3">
-                        <a className="sm-icon" href="https://www.facebook.com/profile.php?id=100022233753912" target="_blank"><i className='bx bxl-facebook'/></a>
+                        <a className="sm-icon" rel="noreferrer" href="https://www.facebook.com/profile.php?id=100022233753912" target="_blank"><i className='bx bxl-facebook'/></a>
                     </span>
                     <span className="mx-3">
-                        <a className="sm-icon" href="https://www.instagram.com/aitolkun_17" target="_blank"><i className='bx bxl-instagram'/></a>
+                        <a className="sm-icon" rel="noreferrer" href="https://www.instagram.com/aitolkun_17" target="_blank"><i className='bx bxl-instagram'/></a>
                     </span>
                 </div>
                 <span>Privacy</span>
